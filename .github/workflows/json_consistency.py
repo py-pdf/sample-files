a6ab755bbbb037bf1d47498b72fea41d17cfe6b5
Martin Thoma <info@martin-thoma.de>
2022-04-03 20:01:01 +0200

    ENH: Add PDF with 4 pages

--- .github/workflows/json_consistency.py
+++ .github/workflows/json_consistency.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import os
 import sys
+from pathlib import Path
 from typing import List
 
 from pydantic import BaseModel, NonNegativeInt
@@ -23,14 +24,24 @@ def main():
     with open("files.json") as f:
         data = json.load(f)
     main_pdf = MainPdfFile.parse_obj(data)
+    registered_pdfs = []
 
     seen_failure = False
     for entry in main_pdf.data:
+        registered_pdfs.append(entry.path)
         if not os.path.exists(entry.path):
-            print(f"File not found: {entry.path}")
+            print(f"❌ ERROR: File not found: {entry.path}")
             seen_failure = True
         else:
-            print(f"Found {entry.path}")
+            print(f"✅ Found {entry.path}")
+
+    # Are all files registered?
+    pdf_paths = Path(".").glob("**/*.pdf")
+    for pdf_path in pdf_paths:
+        if str(pdf_path) not in registered_pdfs:
+            print(f"❌ ERROR: File not registered: {pdf_path}")
+            seen_failure = True
+
     if seen_failure:
         sys.exit(1)
 
